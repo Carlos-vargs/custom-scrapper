3b30d90d4af79e3446a936781dd754d908bf4abf
Carlos-vargs <cvargaslopez769@gmail.com>
2025-11-18 09:16:41 -0600

    feat: add Puppeteer configuration file for browser settings

---
+++ puppeteer.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  cacheDirectory: "/opt/render/.cache/puppeteer",
+  defaultBrowser: "chrome",
+  browsers: [
+    {
+      name: "chrome",
+      channel: "stable",
+    },
+  ],
+};
